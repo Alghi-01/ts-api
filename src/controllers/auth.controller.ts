0bc4708a66c9e0909016e4659c335c786d5144ce
Abdi-01 <abdialghi@gmail.com>
2025-05-15 15:12:46 +0700

    revamp: implement generic types for createResponse template

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { loginUser } from "../services/auth.service";
+import { createResponse } from "../utils/response.util";
 
 export const loginController = async (req: Request, res: Response) => {
   try {
     const userToken = await loginUser(req.body);
 
     const { password, ...safeUser } = userToken;
-    res.status(200).send(safeUser);
+    res.status(200).send(createResponse(true, "Login success", safeUser));
   } catch (error) {
     res.status(500).send(error);
   }
